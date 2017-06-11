0c6e7af83aec07a57cac799ed80c87ba1911c215
disherdesign <drewdisher@fastmail.com>
2017-06-11 13:41:59 -0500

    adding podcast mp3 player

--- Attempt1/untitled folder/romanApp.js
+++ Attempt1/untitled folder/romanApp.js
@@ -1,28 +1,35 @@
-angular.module('romanApp',[])
+angular.module('romanApp',['angularSoundManager'])
+
+.constant('moment', moment)
 
 
-.controller('romanCtrl', function($scope, romanService){
+
+.controller('romanCtrl', function($scope, romanService, moment){
+	$scope.moment = moment
 	$scope.getFeed = function() {
 		romanService.getFeed().then(function(response){
 			$scope.feed = response.data.items;
 			console.log($scope.feed)
 		});
 		
-
+		$scope.cat = ["cat", 'fog', 'blog', 'coffee', "stout"]
 
 
 	}
 
-	   setInterval(function(){
+	   setTimeout(function(){
     $scope.getFeed();
-  }, 10000)
+  }, 100)
 })
 
 .service('romanService', function($http) {
 	this.getFeed = function() {
-		return $http.get('https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.99percentinvisible.org%2F99percentinvisible')
+		return $http.get('https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.99percentinvisible.org%2F99percentinvisible&api_key=jpo2dfep1qddbkbrkkthp6wpxxcvuj6wa3wzbgic&count=150')
 	}
 
 
 
 	})
+
+
+// https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.99percentinvisible.org%2F99percentinvisible
--- untitled folder/romanApp.js
+++ untitled folder/romanApp.js
@@ -1,29 +1,64 @@
 angular.module('romanApp',[])
 
 
-.controller('romanCtrl', function($scope, romanService){
+.constant('moment', moment);
+
+.controller('romanCtrl', function($scope, romanService, moment){
 	$scope.getFeed = function() {
-		romanService.getFeed().then(function(response){
-			$scope.feed = response.data.items;
+		romanService.getFeed().then(function(result){
+			$scope.feed = romanService.formatEpisodeData(result);
 			console.log($scope.feed)
 		});
-		
+	};
 
 
-
-	}
 
 
 	   setTimeout(function(){
     $scope.getFeed();
-  }, 1000)
+  }, 100)
 })
 
-.service('romanService', function($http) {
+.service('romanService', function($http, $q) {
+	 
+
 	this.getFeed = function() {
-		return $http.get('https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.99percentinvisible.org%2F99percentinvisible')
-	}
+		var deferred = $q.defer();
+
+		$http.get('https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.99percentinvisible.org%2F99percentinvisible&api_key=jpo2dfep1qddbkbrkkthp6wpxxcvuj6wa3wzbgic&count=30')
+			.then(function(response){
+				var data = response.data;
+				console.log(data)
+				deferred.resolve(data);
+			});
+			return deferred.promise;
+		};
+	this.formatEpisodeData = function(result){
+		var parsedResults = [];
+
+  		for(var i=0;i<result.length;i++) {
+  			var newObj= {
+  				title:result[i].title,
+  				date:result[i].pubDate
+  			};
+  			parsedResults.push(newObj);
+		}
+		return parsedResults;
+	};
+});
 
 
 
-	})
+	// this.formatDate = function(date){
+	// 	var arr = date.slice(0,10).split("-");
+ //  		var newDate = [];
+ //  		newDate.push(arr[1]);
+ //  		newDate.push(arr[2]);
+ //  		newDate.push(arr[0]);
+ //  		newDate = newDate.join('.');
+ //  		return newDate
+	// }
+
+
+
+	
